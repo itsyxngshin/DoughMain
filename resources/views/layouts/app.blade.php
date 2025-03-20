a6bd7634d8ef30e5ec7a86e228ebdca26bd5efe3
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-03-20 21:03:51 +0800

    build: install AlpineJS for lightweight JS implementation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <!-- Include Bootstrap -->
+    <!-- Include Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    @yield('log_screen') <!-- This should match the section name -->
+<body class="bg-gray-100">
+    @yield('log_screen')
+    @yield('register_screen')
 </body>
 </html>
