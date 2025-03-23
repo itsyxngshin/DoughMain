878f0b5512c91ac3fc6f0312a7239e0e633cd87e
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-03-23 23:36:15 +0800

    fix: correct js file entry in vite configuration setting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- Include Tailwind CSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js']) <!-- Load Vite -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
